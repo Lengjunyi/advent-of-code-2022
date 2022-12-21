f7ac0c1589f4e5b444d0a7b02667ba607b83cb9a
Alan Liang <al2520@ic.ac.uk>
2022-12-21 06:22:51 +0000

    Added optimization

--- aoc_day19.py
+++ aoc_day19.py
@@ -29,7 +29,7 @@ Blueprint 28: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsi
 Blueprint 29: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 5 clay. Each geode robot costs 4 ore and 11 obsidian.
 Blueprint 30: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 20 clay. Each geode robot costs 2 ore and 17 obsidian."""
 
-NN = 32
+NN = 24
 ans = 0
 
 for blueprint in input.split("\n"):
@@ -39,14 +39,16 @@ for blueprint in input.split("\n"):
     res = 0
 
     visited = set()
-    m = 0
-
     from collections import deque
     queue = deque()
-    queue.append((NN,1,0,0,0,0,0,0,0))
+    queue.append((NN,1,0,0,0,0,0,0,0,[]))
     cases = [0] * 40
+
     while queue:
-        (d, a1, a2, a3, a4, r1, r2, r3, r4) = queue.pop()
+        (d, a1, a2, a3, a4, r1, r2, r3, r4, lastRobot) = queue.pop()
+        if r4 + d * a4 + (d-2) * (d - 1) // 2 < res:
+            continue
+        canbuy = []
         for state in range(5):
             dn, a1n, a2n, a3n, a4n, r1n, r2n, r3n, r4n = d-1, a1, a2, a3, a4, r1, r2, r3, r4
             if state == 3:
@@ -75,11 +77,15 @@ for blueprint in input.split("\n"):
                     min(r1n+a1, max(o1, c1, ob1, g1) * dn), 
                     r2n+a2, 
                     r3n+a3, 
-                    r4n+a4
+                    r4n+a4,
+                    tuple(canbuy) if state == 4 else tuple()
                 )
+                if state != 4:
+                    canbuy.append(state)
+                if state in lastRobot:
+                    continue
                 if dn == 0:
                     res = max(res, r4n+a4)
-                    m = max(m, r4n+a4)
                 elif newParams not in visited:
                     visited.add(newParams)
                     queue.append(newParams)
